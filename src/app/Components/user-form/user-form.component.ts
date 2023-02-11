5990be7732a9972b5a722ae6f8f30303474c0e68
Dina Gezaf <106817687+DinaGezaf@users.noreply.github.com>
2023-02-11 22:03:05 +0200

    Initial-Config

--- src/app/Components/user-form/user-form.component.ts
+++ src/app/Components/user-form/user-form.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from '../user.service';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { UserService } from "../user.service";
 
 @Component({
-  selector: 'app-user-form',
-  templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.css'],
+  selector: "app-user-form",
+  templateUrl: "./user-form.component.html",
+  styleUrls: ["./user-form.component.css"],
 })
 export class UserFormComponent implements OnInit {
   userId: any;
   userData: any;
-
+  users: any;
   userForm = new FormGroup({
-    Username: new FormControl('', [
+    Username: new FormControl("", [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(10),
     ]),
-    Email: new FormControl('', [Validators.required, Validators.email]),
-    Password: new FormControl('', [
+    Email: new FormControl("", [Validators.required, Validators.email]),
+    Password: new FormControl("", [
       Validators.required,
       Validators.minLength(7),
       Validators.maxLength(15),
@@ -31,7 +31,7 @@ export class UserFormComponent implements OnInit {
     private userService: UserService,
     public router: Router
   ) {
-    this.userId = this.activatedRoute.snapshot.params['id'];
+    this.userId = this.activatedRoute.snapshot.params["id"];
   }
   ngOnInit(): void {
     if (this.userId) {
@@ -45,6 +45,11 @@ export class UserFormComponent implements OnInit {
       });
     }
   }
+  loadUsers() {
+    this.userService.getAllUsers().subscribe((response) => {
+      this.users = response;
+    });
+  }
   SaveData(e: Event) {
     e.preventDefault();
     this.userData = this.userForm.value;
@@ -52,18 +57,18 @@ export class UserFormComponent implements OnInit {
       this.userService
         .editUser(this.userId, this.userData)
         .subscribe((response) => {
-          alert(`Updated ${this.userData.Username}`);
           this.userData = response;
+          alert(`Updated ${this.userData.Username}`);
+          this.loadUsers();
+          this.router.navigate(["/users"]);
         });
     } else {
       this.userService.addNewUser(this.userData).subscribe((response) => {
-        alert(`Added ${this.userData.Username}`);
         this.userData = response;
+        alert(`Added ${this.userData.Username}`);
+        this.loadUsers();
+        this.router.navigate(["/users"]);
       });
     }
-  }
-
-  backToHome() {
-    this.router.navigate(['/users']);
   }
 }
